6676d43881483c9d57088f018e15734e98a30d23
ErickCO-rep <51710143+ErickCO-rep@users.noreply.github.com>
2019-09-30 22:43:17 -0300

    Escalonamento-SJF.py
    Realizado a correção do algoritmo, print da tabela de processos ao entrar e após a ordenação

--- escalonamento-SJF.py
+++ escalonamento-SJF.py
@@ -1,42 +1,52 @@
-
-
-#FUNÇÃO DO ALGORITMO SJF
+#ALGORITMO SJF
 def sjf ():
+        print("PROCESSOS CRIADOS")
+        print("ID processo  | Entrada do Processo | Tempo de Execução ")
+        for x in range(0,n):
+                print("    ",x+1,"               ",tmpEnt[x],"               ",tmpExe[x])
+        
         entradas = list(tmpEnt)
         tempos = list(tmpExe)
+        identf = list(idproc)
         escolhido = 0
         b=0
         wait=0
         for j in range(0,n): #ORDENA OS TEMPOS(DO MENOR PARA O MAIOR)
                 for i in range(0,n-1):
-                        if tempos[i]>tempos[i+1]: 
+                        if tempos[i]>tempos[i+1]:
                                 Aux = tempos[i+1]#TROCA
                                 tempos[i+1] = tempos[i]
                                 tempos[i] = Aux
-                                temp.append(tempos[i]) 
                                 Auxe = entradas[i+1] #TROCA
                                 entradas[i+1] = entradas[i]
                                 entradas[i] = Auxe
-                        entra.append(entradas[j])
-                        print(entra,"entro")
-
+                                Auxi = identf[i+1]
+                                identf[i+1] = identf[i]
+                                identf[i] = Auxi
+                
+                entra.append(entradas[j])
+                temp.append(tempos[j])
+                identific.append(identf[j])
+                          
         for z in range(0,n):
                 a = float(entradas[z-1]+tempos[z-1])
-                print(a,"soma")
-                print(tempos[z])
                 ent = float(entradas[z])
-                print(ent,"entrada atual")
                 wait = (a-ent)
-                print(z)
                 if(z == 0):
                         wait = entradas[z]
                 if(wait < 0):
                         wait = 0.0
-                print(wait,"espera")
                 tmpEsp.append(wait)
+        print("\n")
+        print("PROCESSOS ORDENADOS E EXECUTADOS")
+        print("ID processo  | Entrada do Processo | Tempo de Execução | Tempo de Espera")
+        for o in range(0,n):
+                       
+                print("     ",identific[o],"             ",entra[o],"               ",temp[o],"              ",tmpEsp[o])
 
         soma = 0
         relogio = 0
+        #calcula turnaround
         for x in range(0,n):
                 for y in range(0,n):
                         if entradas[y] <= relogio and entradas[y]>=0:
@@ -48,35 +58,34 @@ def sjf ():
                 entradas[escolhido]=-1
         return float(soma/n)
 
+#calcula media espera
 def espera_media(lista):
         l = lista
-        some = 0 #iniciando a variavel soma
-        for p in range(len(l)): #variacao de i de acordo com o tamanho de l
-                some += l[p] #soma dos elemenos relacionados ao indice i do laço for
+        some = 0 
+        for p in range(len(l)): 
+                some += l[p]
         media = some/n
         return media
 
 n = int(input ("Informe o numero de processos: "))
+print("\n")
 tmpExe = []
 tmpEnt = []
 tmpEsp = []
+idproc = []
+
 temp = []
 entra = []
+identific = []
 
-#LÊ OS TEMPOS DE EXECUÇÃO E DE ENTRADA PARA CADA PROCESSO
+#Entrada dos processos e seus valores
 for x in range(1,n+1):
         print ("Tempo de entrada do processo ", x, ": ")
         tmpEnt.append(float(input()))
         print ("Tempo de execução do processo ", x, ": ")
         tmpExe.append(float(input()))
-        
+        idproc.append(x)
+
+print ("\n")
 print ("TURNAROUND MEDIO: ", sjf())
-print ("TEMPO MEDIO DE ESPERA", espera_media(tmpEsp))
-
-for x in range(0,n):
-        print("ID processo  | Entrada do Processo | Tempo de Execução | Tempo de Espera")
-        print("    ",x+1,"               ",tmpEnt[x],"               ",tmpExe[x],"               ",tmpEsp[x])
-        print()
-
-pass
-
+print ("TEMPO DE ESPERA MEDIO: ", espera_media(tmpEsp))
